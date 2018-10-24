673b8b1c6c420c4d00c8ea4b7f5240b7a3e3c8a1
Milena Cabrera Patino <mcabrerapatino@gmail.com>
2018-10-23 23:11:32 -0500

    Prove of concept CSSTransitionGroup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { CSSTransitionGroup } from 'react-transition-group'
+
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {items: ['hello', 'world', 'click', 'me']}
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleAdd() {
+    const newItems = this.state.items.concat([
+      prompt('Enter some text')
+    ])
+    this.setState({items: newItems})
+  }
+
+  handleRemove(i) {
+    let newItems = this.state.items.slice()
+    newItems.splice(i, 1)
+    this.setState({items: newItems})
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    const items = this.state.items.map((item, i) => (
+      <div key={item} onClick={() => this.handleRemove(i)}>
+        {item}
       </div>
-    );
+    ))
+    return (
+      <div>
+      <button onClick={this.handleAdd}>Add Item</button>
+      <CSSTransitionGroup
+        transitionName="example"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}>
+        {items}
+      </CSSTransitionGroup>
+    </div>
+    )
   }
 }
 
-export default App;
+export default App
